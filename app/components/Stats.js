005df8a09dcc8ccb5e7ec69e41648b0fe5f0afd8
Andy Ramaroson <andyramaroson@gmail.com>
2023-12-10 18:52:58 +0100

    [fix] error

--- app/components/Stats.js
+++ app/components/Stats.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { BiChevronRight } from 'react-icons/bi'
+// import { BiChevronRight } from 'react-icons/bi'
 
 const statContent = {
   stats: [
